35143c2b09de9586d1ad575e8e60da8f683519c1
Lev777 <borichevskiy@gmail.com>
2023-05-18 20:25:18 +0200

    Find pair sum: Added test cases and notes

--- src/find_pair_sum.rs
+++ src/find_pair_sum.rs
@@ -13,18 +13,25 @@
 // Input: vec = [5, 2, 6, 8, 1, 9], sum = 12
 // Output: None
 
+//Notes:
+// Data structures: HashSet
+// Implementation: Loop through the vector and subtract element from the sum
+// then check if HashSet has this value return pair, if not add to the HashSet
+
 use std::collections::HashSet;
 
 #[allow(dead_code)]
-fn find_pair_sum(numbers: &Vec<i32>, sum: i32) -> Option<(i32,i32)> {
+fn find_pair_sum(numbers: &Vec<i32>, sum: i32) -> Option<(i32, i32)> {
     let mut set = HashSet::new();
 
     for e in numbers {
         let another = sum - e;
 
         match set.get(&another) {
-            Some( value) => return Some((*e, *value)),
-            _ => { set.insert(*e); }
+            Some(value) => return Some((*e, *value)),
+            _ => {
+                set.insert(*e);
+            }
         }
     }
     None
@@ -33,12 +40,25 @@ fn find_pair_sum(numbers: &Vec<i32>, sum: i32) -> Option<(i32,i32)> {
 #[cfg(test)]
 mod test {
     use super::*;
-    
+
     #[test]
-    fn find_a_pair() {
+    fn test_case_1() {
         let numbers = vec![8, 7, 2, 5, 3, 1];
         let sum = 10;
-        assert_eq!(find_pair_sum(&numbers, sum), Some((2,8)));
-        println!("{:?}", numbers);
+        assert_eq!(find_pair_sum(&numbers, sum), Some((2, 8)));
+    }
+
+    #[test]
+    fn test_case_2() {
+        let numbers = vec![5, 2, 6, 8, 6, 9];
+        let sum = 12;
+        assert_eq!(find_pair_sum(&numbers, sum), Some((6, 6)));
+     }
+
+    #[test]
+    fn test_case_3() {
+        let numbers = vec![5, 2, 6, 8, 1, 9];
+        let sum = 12;
+        assert_eq!(find_pair_sum(&numbers, sum), None);
     }
 }
