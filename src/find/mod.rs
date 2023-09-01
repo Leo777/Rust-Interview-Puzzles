6a3b2af170d050d0f652fbb3f1011553d289a418
Leo777 <prometeus555@proton.me>
2023-09-01 11:55:34 +0200

    refactoring

---
+++ src/find/mod.rs
@@ -0,0 +1,10 @@
+pub mod find_pair_sum;
+pub mod find_max_product_of_two_numbers;
+pub mod find_longest_slice_with_a_given_sum;
+pub mod find_equilibrium;
+pub mod find_majority_element_in_vec;
+pub mod find_pairs_with_provided_diff;
+pub mod find_the_lowest_index_of_rep_element;
+pub mod find_pivot_index;
+pub mod find_common_prefix;
+pub mod find_missing_natural_number;
