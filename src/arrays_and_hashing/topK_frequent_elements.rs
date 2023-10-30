f1e1da8ca6255e035a32e3a08cad259b13dac3fd
Leo777 <borichevskiy@gmail.com>
2023-10-30 16:08:27 +0100

    top_k element

---
+++ src/arrays_and_hashing/topK_frequent_elements.rs
@@ -0,0 +1,37 @@
+// Problem
+// Given an integer array nums and an integer k, return the k most frequent elements.
+//  You may return the answer in any order.
+
+
+//Notes: Use bucket sort. Element count will be index and 
+
+// Complexity:
+// Time: O(n)
+// Space: O(n)
+
+#[allow(dead_code)]
+pub fn two_sum(nums: Vec<i32>, k: i32) -> Vec<i32> {
+    let mut map = std::collections::HashMap::new();
+
+    for &e in nums.iter() {
+        *map.entry(e).or_insert(0) += 1;
+    }
+    let mut sorted: Vec<Vec<i32>> = vec![Vec::new(); nums.len()];
+    for (&e, &count) in map.iter() {
+        sorted[nums.len() - count].push(e);
+    }
+    print!("{:?}", sorted);
+    sorted.into_iter().flatten().take(k as usize).collect()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_case_1() {
+        let nums = vec![1,1,1,2,2,3];
+        let k = 2;
+        assert_eq!(two_sum(nums, k), vec![1, 2]);
+    }
+}
